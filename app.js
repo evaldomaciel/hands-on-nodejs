4add7b2231efd0793a881fbcc13b462b7da4314e
Evaldo Maciel <evaldomaciel17@gmail.com>
2022-06-12 17:44:29 -0300

    Configuração inicial das branches

--- app.js
+++
@@ -1,6 +0,0 @@
-import talk from "./talk.js";
-import { talk as _talk, walk } from "./guy.js";
-
-talk("Fala, meu camarada, chegou até aqui sem pular o Hello World?");
-_talk("Opa, claro!");
-walk("Vou para casa");
